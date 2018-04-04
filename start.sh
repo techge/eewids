3a79633c04c4287220ba1eaef519d71fe1497eeb
techge <techge+github@posteo.net>
2018-04-04 10:02:59 +0200

    docker-compose is finally working

--- start.sh
+++ start.sh
@@ -6,26 +6,22 @@ then
     sudo systemctl start docker
 fi
 
-echo "Starting RabbitMQ"
-docker run -tid --net=host rabbitmq
+echo "docker-compose up -d ..."
+docker-compose up -d
 
-echo "Starting Kismet Stuff"
-docker run -tid --net=host eewids/kismet
-sleep 10
+sleep 3
+echo "starting kismet remote capture ..."
 /usr/bin/kismet_cap_linux_wifi --connect localhost:3501 --source=wlp2s0 &
 
-echo "Starting kismet-capture2rabbit"
-docker run -tid --net=host eewids/kiscap2rabbit
-
-echo "Starting Rabbit-recv"
-docker run -ti --net=host rabbit-recv
+echo "attaching to rabbit-recv"
+docker-compose logs --follow rabbit-recv
 
 echo "killing everything :)"
-docker stop $(docker ps -q)
+docker-compose down
 kill %%
 
 echo "I am just waiting some time..."
-sleep 5
+sleep 3
 
 echo "Try to restore WiFi"
 nmcli device set wlp2s0 managed true
