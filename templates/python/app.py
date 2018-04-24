dc27cc87a4ddc0ac53917611ecbbdb9a1b427d3b
techge <techge+github@posteo.net>
2018-04-24 10:24:21 +0200

    template draft finished

--- templates/python/app.py
+++ templates/python/app.py
@@ -1,45 +1,65 @@
 #!/usr/bin/python
 
 '''
-This file is part of EEWIDS 
+    This file is part of EEWIDS (Easily Expandable WIDS)
 
-EEWIDS is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
+    Copyright (C) 2018 Alexander Paetzelt <techge+eewids posteo net>
 
-EEWIDS is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 
+import sys
 import pika
 import json
 import argparse
 
 
-# Choose the captured frames you want to get for your detection app by uncommenting here
-routing_key_table = [ 
+# TODO Choose the captured frame type, if you want to get *all* frames of this type (look below otherwise)
+type_table = [
+
+    #"Management",
+    #"Control",
+    #"Data",
+    #"Extension",
+
+    ]
+
+# TODO Choose the captured frame subtype you want to get for your detection app by uncommenting here
+subtype_table = [ 
+
     ## Management frames
     #"Association Request",
     #"Association Response",
     #"Reassociation Request",
     #"Reassociation Response",
     #"Probe Request",
-    "Probe Response",
-    "Beacon",
+    #"Probe Response",
+    #"Timing Advertisment",
+    #"Beacon",
     #"ATIM",
     #"Disassociation",
     #"Authentication",
     #"Deauthentication",
     #"Action",
+    #"Action No Ack",
+
     ## Control frames
+    #"Beamforming Report Poll",
+    #"VHT NDP Announcement",
+    #"Control Frame Extension",
+    #"Control Wrapper",
     #"Block Ack Request",
     #"Block Ack",
     #"PS-Poll",
@@ -48,6 +68,7 @@ routing_key_table = [
     #"ACK",
     #"CF-end",
     #"CF-end + CF-ack",
+
     ## Data frames
     #"Data",
     #"Data + CF-ack",
@@ -64,22 +85,66 @@ routing_key_table = [
     #"QoS Null",
     #"QoS + CF-poll (no data)",
     #"QoS + Cf-ack (no data)",
-]
+
+    ## Extension
+    #"DMG Beacon",
+    ]
 
 
 def detect_attack(data):
 
-    print(" [x] Captured %s %r" % (data['subtype'][1], data['ESSID']))
+    detected = False
+    
+    # TODO
+    # This is the main part you have to think about. Here is your main code.
+    # data is a python dictionary which contains all fields described on
+    # https://github.com/techge/eewids/blob/master/docs/rabbitmq/kismet_capture-fields.md
+    # TODO
 
-    send_alert()
+    if detected:
+
+        # TODO 
+        # create alert messages based on fields described on
+        # https://github.com/techge/eewids/blob/master/docs/rabbitmq/messages-fields.md
+        # TODO
+        alertname = 'SomethingTerrible'
+        description = 'SomethingTerrible detected...'
+        # ...
+        
+        alert = [
+                'name': alertname
+                'description': description
+                # ...
+                ]
+
+        send_alert(alert)
 
 
-def send_alert():
+def send_alert(alert):
 
-    print("Alert sent.")
+    channel = connection.channel()
+    channel.exchange_declare(exchange='messages',
+                             exchange_type='topic')
+    
+    message = json.dumps(alert)
+
+    # TODO adapt routing_key; format is servicename.messagetype
+    # see https://github.com/techge/eewids/blob/master/docs/rabbitmq/messages-topics.md
+    routing = 'app.alert' 
+    channel.basic_publish(exchange='kismet_capture',
+                          routing_key=routing,
+                          body=message)
+    
+    print(" [x] Alert %r sent.", % message)
+    connection.close()
 
 
 def main(rab_host, rab_port):
+
+    # sanity check
+    if (!type_table and !subtype_table):
+        print("No frame type or subtype choosen, please adapt type tables to your needs!")
+        sys.exit()
 
     ConnectionParameters = pika.ConnectionParameters(host=rab_host,
                                                      port=rab_port,
@@ -88,16 +153,27 @@ def main(rab_host, rab_port):
     channel = connection.channel()
 
     channel.exchange_declare(exchange='kismet_capture',
-                             exchange_type='direct')
+                             exchange_type='topic')
 
+    # TODO change exclusive to False if your detection can get split between multiple workers
     result = channel.queue_declare(exclusive=True)
     queue_name = result.method.queue
 
-    for key in routing_key_table:
+    # queue binding per frame type choosen above
+    for key in type_table:
 
-        channel.queue_bind(exchange='kismet_capture',
-                           queue=queue_name,
-                           routing_key=key)
+        routing_key = '*.' + key + '.*'
+        channel.queue_bind('kismet_capture',
+                           queue_name,
+                           routing_key)
+
+    # queue binding per frame subtype choosen above
+    for key in subtype_table:
+
+        routing_key = '*.*.' + key
+        channel.queue_bind('kismet_capture',
+                           queue_name,
+                           routing_key)
 
     def callback(ch, method, properties, body):
 
