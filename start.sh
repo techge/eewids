c25d0a07fd78a9e048773e31c8c9d9fc3bb934e7
techge <techge+github@posteo.net>
2018-06-06 16:03:41 +0200

    Improvements for getting Kismet running without needing to build remote cap, still not perfect though

--- start.sh
+++ start.sh
@@ -1,37 +1,60 @@
 #!/bin/sh
 
-# TODO check if docker must be run with root privs
-
-# TODO add option for using remote capture with docker container
-
 if [ $# -lt 1 ]; then
-    echo "Usage: ./start.sh [wifi-interface]"
     echo
-    echo "e.g. ./start.sh wlan0"
+    echo "Usage: sudo ./start.sh [wifi-interface | --server]"
     echo
+    echo "e.g. sudo ./start.sh wlan0        # for standalone version"
+    echo "or   sudo ./start.sh --server     # only start Kismet server"
+    echo
+    exit
+fi
+
+if [ "$EUID" -ne 0 ]
+    then echo "Please run as root or with 'sudo'"
     exit
 fi
 
 if [ \! -e "/var/run/docker.sock" ]
 then
-    echo "sudo systemctl start docker"
-    sudo systemctl start docker
+    echo "systemctl start docker"
+    systemctl start docker
 fi
 
-echo "docker-compose up -d ..."
-docker-compose up -d
+if [ "$1" == "--server" ]
+then
 
-echo "starting kismet remote capture ..."
-sleep 3
-/usr/bin/kismet_cap_linux_wifi --connect localhost:3501 --source=$1 &
+    echo "docker-compose up -d ..."
+    docker-compose up -d
 
-docker-compose logs --follow logprint
+    docker-compose logs --follow logprint
+    # you could now connect to the kismet server via
+    # /usr/bin/kismet_cap_linux_wifi --connect localhost:3501 --source=wlan0
 
-echo "killing everything now :)"
-docker-compose down
+    echo "killing everything now :)"
+    docker-compose down
 
-kill %%
+else
 
-echo "Try to restore WiFi"
-sleep 3
-nmcli device set $1 managed true
+    echo "docker-compose up ..."
+    docker-compose -f docker-compose.yml \
+                   -f docker-compose.standalone.yml up -d
+
+    echo "connecting interface with kismet..."
+    sleep 3
+    # inspired by Kismet wireless...
+    # https://github.com/kismetwireless/kismet/blob/master/packaging/docker/assign_wifi_to_docker.sh
+    PHY=$(cat /sys/class/net/"$1"/phy80211/name)
+    PID=$(docker container inspect -f '{{.State.Pid}}' eewids_kismet-server_1)
+    iw phy "$PHY" set netns "$PID"
+
+    docker-compose logs --follow logprint
+
+    echo "killing everything now :)"
+    docker-compose down
+
+    echo "Try to restore WiFi (based on NetworkManager)"
+    sleep 3
+    nmcli device set $1 managed true
+
+fi
